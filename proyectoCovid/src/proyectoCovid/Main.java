95bbdf6efbf4d684419d6189f4ccce5a8bf2ce67
Adrian George Olteanu <agolteanu01@educarex.es>
2021-05-20 09:20:29 +0200

    Primera versión del proyecto. Solo contiene un saludo.

---
+++ proyectoCovid/src/proyectoCovid/Main.java
@@ -0,0 +1,13 @@
+package proyectoCovid;
+/*
+ * @author Adrian George Olteanu
+ * @version 1.0.0
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es una aplicación de covid 19");
+	}
+
+}
