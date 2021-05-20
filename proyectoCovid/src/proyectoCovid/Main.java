02f1d2bc205c7eed438c005283d7690ce185968d
Adrian George Olteanu <agolteanu01@educarex.es>
2021-05-20 09:38:55 +0200

    Se ha añadido una funcionalidad clave para que este método realice su tarea. IMPRIMIR

--- proyectoCovid/src/proyectoCovid/Main.java
+++ proyectoCovid/src/proyectoCovid/Main.java
@@ -8,6 +8,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es una aplicación de covid 19");
+		System.out.println("PROBANDO.....");
+		System.out.println("PROBANDO1....");
+		System.out.println("PROBANDO.2...");
+		System.out.println("PROBANDO..3..");
+		System.out.println("PROBANDO...4.");
+		System.out.println("PROBANDO....5");
 	}
 
 }
